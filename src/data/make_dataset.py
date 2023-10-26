ebb00b7667e09fd7c45e75b4d9abecd33b33e437
Eric Ji <ericji3@illinois.edu>
2023-10-26 17:27:50 -0500

    adding evalulation for all datasets code

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -29,3 +29,36 @@ def import_data():
     test_loader = DataLoader(dataset=test_dataset, batch_size=16, shuffle=False, num_workers=6)
 
     return train_loader, val_loader, test_loader
+
+def import_testsets():
+    transform = transforms.Compose([
+            transforms.ToTensor(),
+            concat_fft(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406, 0],
+                                 std= [0.229, 0.224, 0.225, 1]),
+        ])
+
+    test_dataset0 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/ADM', transform = transform)
+    test_dataset1 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/DDPM', transform = transform)
+    test_dataset2 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/Diff-ProjectedGAN', transform = transform)
+    test_dataset3 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/Diff-StyleGAN2', transform = transform)
+    test_dataset4 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/IDDPM', transform = transform)
+    test_dataset5 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/LDM', transform = transform)
+    test_dataset6 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/PNDM', transform = transform)
+    test_dataset7 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/ProGAN', transform = transform)
+    test_dataset8 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/ProjectedGAN', transform = transform)
+    test_dataset9 = ImageFolder(root='../../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/test/StyleGAN', transform = transform)
+
+    test_loader = np.empty(10)
+    test_loader[0] = DataLoader(dataset=test_dataset0, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[1] = DataLoader(dataset=test_dataset1, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[2] = DataLoader(dataset=test_dataset2, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[3] = DataLoader(dataset=test_dataset3 batch_size=16, shuffle=False, num_workers=6)
+    test_loader[4] = DataLoader(dataset=test_dataset4, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[5] = DataLoader(dataset=test_dataset5, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[6] = DataLoader(dataset=test_dataset6, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[7] = DataLoader(dataset=test_dataset7, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[8] = DataLoader(dataset=test_dataset8, batch_size=16, shuffle=False, num_workers=6)
+    test_loader[9] = DataLoader(dataset=test_dataset9, batch_size=16, shuffle=False, num_workers=6)
+
+    return test_loader
---
+++ src/eval_model.py
@@ -0,0 +1,55 @@
+import os
+import yaml
+import torch
+import wandb
+import argparse
+import numpy as np
+from tqdm import tqdm
+from architectures import ResNet18
+from data import make_dataset
+
+wandb.login(key="76c1f7f13f849593c4dc0d5de21f718b76155fea")
+wandb.init(project='2D-FACT')
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+def main():
+    #import data
+    test_loader = make_dataset.import_testsets()
+    data_name = {"ADM", "DDPM", "Diff-ProjectedGAN", "Diff-StyleGAN2", "IDDPM", "LDM", "PNDM", "ProGAN", "ProjectedGAN", "StyleGAN"}
+
+    #create command line interface
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="Name of trained weights")
+    args = parser.parse_args()
+
+    #import model
+    model = ResNet18().to(device)
+    save_path = os.path.join('../models','2D-FACT_'+args.model)
+    model.load_state_dict(torch.load(save_path))
+    
+    #evalulate on each testset
+    for idx in range(test_loader):
+        test_accuracy = eval(test_loader[idx], model)
+        wandb.log({'Dataset': data_name[idx], 'Test accuracy': test_accuracy})
+
+def eval(data_loader, model):
+    model.eval()
+    correct = 0
+    total = 0
+
+    it_test = tqdm(enumerate(data_loader), total=len(data_loader), desc="Validating ...", position = 1)
+    for i, (images, labels) in it_test:
+      images, labels = images.to(device), labels.to(device)
+      with torch.no_grad():
+        output = model(images)
+      preds = torch.argmax(output, dim=-1)
+      correct += (preds == labels).sum().item()
+      total += len(labels)
+
+    accuracy = correct / total
+    return accuracy
+
+if __name__ == "__main__":
+    main()
