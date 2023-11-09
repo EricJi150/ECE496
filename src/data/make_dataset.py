fb3fef7a78c15db351deedc2bbdee7ad06254d71
Eric Ji <ericji3@illinois.edu>
2023-11-09 00:25:29 -0600

    transform order

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -15,8 +15,8 @@ class concat_fft:
 def import_data(dataset):
     transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std= [0.229, 0.224, 0.225]),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                 std= [0.5, 0.5, 0.5]),
             concat_fft(),    
         ])
 
