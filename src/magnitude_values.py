2a09cbe228049eac6faef2a9f82012eddd3d513c
Eric Ji <73372943+EricJi150@users.noreply.github.com>
2023-11-15 14:29:59 -0600

    Update magnitude_values.py

--- src/magnitude_values.py
+++ src/magnitude_values.py
@@ -10,11 +10,20 @@ Finding range of values between all channels after transforms
 '''
 class concat_fft:
   def __call__(self, image):
+    # grayimage = transforms.Grayscale(num_output_channels=1)(image)
+    # fft = np.fft.fftshift(np.fft.fft2(grayimage.numpy()))
+    # magnitude = torch.from_numpy(np.abs(fft)).float()
+    # tensor = torch.cat((image,magnitude), dim = 0)
+    # return tensor
+
     grayimage = transforms.Grayscale(num_output_channels=1)(image)
     fft = np.fft.fftshift(np.fft.fft2(grayimage.numpy()))
     magnitude = torch.from_numpy(np.abs(fft)).float()
-    tensor = torch.cat((image,magnitude), dim = 0)
-    return tensor
+    phase = torch.from_numpy(np.angle(fft)).float()
+    tensor1 = torch.cat((image,magnitude), dim = 0)
+    tensor2 = torch.cat((tensor1,phase))
+
+    return tensor2
   
 def main():
     wandb.login(key="76c1f7f13f849593c4dc0d5de21f718b76155fea")
@@ -22,16 +31,16 @@ def main():
 
     transform = transforms.Compose([
             transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std= [0.229, 0.224, 0.225]),
             concat_fft(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406, 0],
-                                 std= [0.229, 0.224, 0.225, 1]),
         ])
 
     train_dataset = ImageFolder(root='../../../../../shared/rsaas/common/diffusion_model_deepfakes_lsun_bedrooms/diffusion_model_deepfakes_lsun_bedroom/train/LDM', transform = transform)
 
-    channel_sums = np.zeros(4)
-    channel_mins = np.ones(4) * float('inf')
-    channel_maxs = np.ones(4) * float('-inf')
+    channel_sums = np.zeros(5)
+    channel_mins = np.ones(5) * float('inf')
+    channel_maxs = np.ones(5) * float('-inf')
 
     count = 0
     num_pixels = 0
@@ -43,14 +52,14 @@ def main():
         
         num_pixels += train_X.size(1) * train_X.size(2)
 
-        for channel in range(4):
+        for channel in range(5):
             data = train_X[channel, :, :]
             channel_sums[channel] += data.sum().item()
             channel_mins[channel] = min(channel_mins[channel], data.min().item())
             channel_maxs[channel] = max(channel_maxs[channel], data.max().item())
     
     
-    channels=["Red", "Green", "Blue", "Magnitude"]
+    channels=["Red", "Green", "Blue", "Magnitude", "Phase"]
     table = wandb.Table(columns=["Channel", "Min", "Max", "Avg"])
     
     for i, channel in enumerate(channels):
