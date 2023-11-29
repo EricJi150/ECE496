79331290ec3a8d852f5fdac232f6c05588109a6e
Eric Ji <ericji3@illinois.edu>
2023-11-29 02:27:04 -0600

    debug

--- src/architectures.py
+++ src/architectures.py
@@ -35,7 +35,7 @@ Modified ResNet-18 to support multiclass 4 channel input
 '''
 class ResNet18_4_Multi(nn.Module):
     def __init__(self):
-        super(ResNet18_4, self).__init__()
+        super(ResNet18_4_Multi, self).__init__()
         self.model = torchvision.models.resnet18()
         self.model.conv1 = torch.nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.model.fc = nn.Linear(self.model.fc.in_features, 11)
