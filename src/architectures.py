f6ad0057f51c1962faca3329951bf0e0b0f96ed4
Eric Ji <ericji3@illinois.edu>
2023-11-30 02:28:37 -0600

    five

--- src/architectures.py
+++ src/architectures.py
@@ -43,3 +43,17 @@ class ResNet18_4_Multi(nn.Module):
     def forward(self, x):
         x = self.model(x)
         return x
+    
+'''
+Modified ResNet-18 to support multiclass 5 channel input
+'''
+class ResNet18_5_Multi(nn.Module):
+    def __init__(self):
+        super(ResNet18_5_Multi, self).__init__()
+        self.model = torchvision.models.resnet18()
+        self.model.conv1 = torch.nn.Conv2d(5, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.fc = nn.Linear(self.model.fc.in_features, 11)
+    
+    def forward(self, x):
+        x = self.model(x)
+        return x
