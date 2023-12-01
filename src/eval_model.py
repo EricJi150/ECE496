9c044c48c9ef39d3a02e2d90c9d04eff36fe4fd4
Eric Ji <ericji3@illinois.edu>
2023-12-01 01:44:01 -0600

    confusion

--- src/eval_model.py
+++ src/eval_model.py
@@ -4,9 +4,13 @@ import torch
 import wandb
 import argparse
 import numpy as np
+import seaborn as sns
 from tqdm import tqdm
-from architectures import ResNet18_5
 from data import make_dataset
+import matplotlib.pyplot as plt
+from architectures import ResNet18_5
+from sklearn.metrics import confusion_matrix
+
 
 wandb.login(key="76c1f7f13f849593c4dc0d5de21f718b76155fea")
 wandb.init(project='2D-FACT-Normalization-Tests')
@@ -52,5 +56,46 @@ def eval(data_loader, model):
     accuracy = correct / total
     return accuracy
 
+def confusion():
+   #import data
+    _, _, test_loader = make_dataset.import_train_multi()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="Name of config file")
+    parser.add_argument("model", help="Name of model")
+    args = parser.parse_args()
+    print(args.config, args.model)
+
+    #import model
+    model = ResNet18_5().to(device)
+    save_path = os.path.join('../models','2D-FACT_'+args.config+'_'+args.model)
+    model.load_state_dict(torch.load(save_path))
+
+    #evaluate
+    model.eval()
+    true_labels = []
+    pred_labels = []
+
+    for data, label in test_loader:
+      data, label = data.to(device), label.to(device)
+      with torch.no_grad():
+        output = model(data)
+      pred = torch.argmax(output, dim=-1)
+      true_labels.extend(label.cpu().numpy())
+      pred_labels.extend(pred.cpu().numpy())
+    
+    #generate matrix
+    matrix = confusion_matrix(true_labels, pred_labels)
+
+    #visualize the matrix
+    plt.figure(figsize=(10,10))
+    sns.heatmap(matrix, annot=True, fmt="d")
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+    #save the matrix
+    plt.savefig('confusion_matrix.png', dpi=300, bbox_inches='tight', facecolor='white')
+
 if __name__ == "__main__":
-    main()
+    confusion()
