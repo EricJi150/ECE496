40c9d9e8d5fcce1bdf915a988fc1267587cfe2e9
Eric Ji <ericji3@illinois.edu>
2023-12-14 00:31:38 -0600

    five

--- src/roc_curve.py
+++ src/roc_curve.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix
 import torch
 import torch.nn as nn
 from tqdm import tqdm
-from architectures import ResNet18_5
+from architectures import ResNet18_2
 from data import make_dataset
 
 
@@ -89,7 +89,7 @@ def full_test(model, test_dataloader, mode = "Full", save_to_file = None):
 
 def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    model =  ResNet18_5().to(device)
+    model =  ResNet18_2().to(device)
     save_path = os.path.join('../models','Shadows'+'two'+'_'+'outdoor')
     model.load_state_dict(torch.load(save_path))
     # train_loader, val_loader, test_loader = make_dataset.import_indoor_data()
