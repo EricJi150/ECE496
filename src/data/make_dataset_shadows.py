3586c69785b3c36eb140667e334122e09abc4b77
Eric Ji <ericji3@illinois.edu>
2024-01-17 06:48:23 -0600

    unconfident set

--- src/data/make_dataset_shadows.py
+++ src/data/make_dataset_shadows.py
@@ -127,10 +127,10 @@ def import_data():
     random.shuffle(val_image_paths)
 
 
-    for data_path in glob.glob(kadinsky_outdoor_path + '/*'):
+    for data_path in glob.glob(firefly_outdoor_path + '/*'):
         classes.append(data_path.split('/')[-1])
         if mode == 'streets':
-            kadinsky_outdoor_image_paths.append(glob.glob(data_path + '/*'))
+            firefly_outdoor_image_paths.append(glob.glob(data_path + '/*'))
 
     # for data_path in glob.glob(mapv_test_path + '/*'):
     #     if mode == 'streets':
@@ -146,7 +146,7 @@ def import_data():
     #     if mode == 'streets':
     #         livingroom_image_paths.append(glob.glob(data_path + '/*'))
 
-    test_image_paths = kadinsky_outdoor_image_paths
+    test_image_paths = firefly_outdoor_image_paths
     test_image_paths = list(flatten(test_image_paths))
 
 
