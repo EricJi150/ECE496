fc10c8587cadd8e4f3871dc72b1b1a74d50a343b
Eric Ji <ericji3@illinois.edu>
2024-01-23 22:26:38 -0600

    50 no pretrain

--- src/unconfident.py
+++ src/unconfident.py
@@ -138,7 +138,7 @@ def full_test(model, test_dataloader, save_to_file = None, title = "title"):
 
 def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    model =  ResNet50_2().to(device)
+    model =  ResNet18_2().to(device)
     save_path = os.path.join('../models','Shadows'+'_'+'outdoor'+'_'+'18')
     model.load_state_dict(torch.load(save_path))
     test_loader = make_dataset_shadows.import_test_data()
