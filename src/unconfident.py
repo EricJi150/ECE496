e2e862db913e0092551c4195cf8a2c87993d5576
Eric Ji <ericji3@illinois.edu>
2024-02-12 15:16:24 -0600

    fix

--- src/unconfident.py
+++ src/unconfident.py
@@ -169,11 +169,12 @@ def main():
     model =  ResNet50_2().to(device)
     save_path = '../models/Shadows/Shadows_deepfloyd_indoor_large'
     # save_path = '../models/Shadows/Shadows_kandinsky_indoor_large'
+    # save_path = '../models/Shadows/Shadows_indoor_50'
     model.load_state_dict(torch.load(save_path))
     # test_loader = make_dataset_shadows.import_test_data()
     # train_loader, val_loader, test_loader = make_dataset_shadows.import_deepfloyd_indoor_large_data()
     train_loader, val_loader, test_loader = make_dataset_shadows.import_kandinsky_indoor_large_data()
-    full_test(model, test_loader, title= 'Deepfloyd Indoor Large eval on Deepfloyd Indoor Large', save_to_file="shadows/roc/FFT_Deepfloyd_Indoor2")
+    full_test(model, test_loader, title= 'Deepfloyd Indoor Large eval on Kandinsky Indoor Large', save_to_file="shadows/roc/FFT_Deepfloyd_Indoor2")
 
 if __name__ == "__main__":
     main()
